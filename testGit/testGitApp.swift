555f2c8ce4827640e4a9b9fc65d59e0a504582a3
Waytoon <flupush@gmail,com>
2022-09-16 10:48:36 +0800

    Initial Commit

---
+++ testGit/testGitApp.swift
@@ -0,0 +1,17 @@
+//
+//  testGitApp.swift
+//  testGit
+//
+//  Created by Ryan Liu on 2022/9/16.
+//
+
+import SwiftUI
+
+@main
+struct testGitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
